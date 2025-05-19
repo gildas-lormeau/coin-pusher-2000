b1af95dc931a43f63530df834a1349c255f48c31
Gildas <gildas.lormeau@gmail.com>
2025-05-19 11:32:22 +0200

    improve autoplay

--- components/cabinet.js
+++ components/cabinet.js
@@ -275,7 +275,7 @@ export default class {
     }
 
     #autoplay() {
-        if (this.#pusher.position.z > -.255 && this.#pusher.position.z < -0.252 && this.#pusher.position.z > this.lastPusherPosition) {
+        if (this.#pusher.position.z > -.275 && this.#pusher.position.z < -0.272 && this.#pusher.position.z > this.lastPusherPosition) {
             Coins.dropCoins({ count: 2 });
         }
         this.lastPusherPosition = this.#pusher.position.z;
