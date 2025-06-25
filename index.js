2194c32bfc2bdc7e8b2728c10bb864107febb5a3
Gildas <gildas.lormeau@gmail.com>
2025-06-25 02:37:18 +0200

    move code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import Game from "./components/game.js";
 
 Game.DEBUG_EMPTY_POOL = 0
 Game.DEBUG_AUTOPLAY = 0
-Game.DEBUG_COLLIDERS = 0
 Game.DEBUG_MAX_SPEED = 0
+Game.DEBUG_COLLIDERS = 0
 Game.DEBUG_HIDE_CABINET = 0
 Game.DEBUG_POLYGONS = 0
 Game.DEBUG_CONTROLS = 1
