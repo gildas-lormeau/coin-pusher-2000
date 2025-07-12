7c7faaea6cc0aa14be97c8f8b7c838d375cbc6db
Gildas <gildas.lormeau@gmail.com>
2025-07-12 07:37:46 +0200

    load state

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as rapier from "@dimforge/rapier3d-simd";
 import Game from "./components/game.js";
 
-Game.DEBUG_EMPTY_POOL = 1;
+Game.DEBUG_EMPTY_POOL = 0;
 Game.DEBUG_AUTOPLAY = 0;
 Game.DEBUG_MAX_SPEED = 1;
 Game.DEBUG_COLLIDERS = 0;
