a1de8af469dd61363ab8d4d2bb495c38521b430b
Gildas <gildas.lormeau@gmail.com>
2025-07-12 07:39:28 +0200

    disable max speed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Game from "./components/game.js";
 
 Game.DEBUG_EMPTY_POOL = 0;
 Game.DEBUG_AUTOPLAY = 0;
-Game.DEBUG_MAX_SPEED = 1;
+Game.DEBUG_MAX_SPEED = 0;
 Game.DEBUG_COLLIDERS = 0;
 Game.DEBUG_HIDE_CABINET = 0;
 Game.DEBUG_POLYGONS = 0;
